9fb8300a69b352c5771839d1eaae72fc2a99c812
yjh920127 <yjh920127@gmail.com>
2022-01-02 15:27:48 +0900

    #1 프로젝트 생성

---
+++ src/main/java/com/example/kukyemarketclone/KuKyeMarketCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.kukyemarketclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuKyeMarketCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuKyeMarketCloneApplication.class, args);
+    }
+
+}
