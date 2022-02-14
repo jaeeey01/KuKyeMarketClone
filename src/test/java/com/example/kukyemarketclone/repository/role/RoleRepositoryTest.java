a89c35a4779d993d7a593a845d6289baa11a4a46
유재현 <yjh920127@gmail.com>
2022-02-14 16:22:29 +0900

    #2 로그인1 - 엔티티 설계 추가 - RoleRepositoryTest

---
+++ src/test/java/com/example/kukyemarketclone/repository/role/RoleRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.example.kukyemarketclone.repository.role;
+
+import com.example.kukyemarketclone.entity.member.Role;
+import com.example.kukyemarketclone.entity.member.RoleType;
+import com.example.kukyemarketclone.exception.RoleNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+@DataJpaTest
+public class RoleRepositoryTest {
+    @Autowired RoleRepository roleRepository;
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void createAndReadTest(){
+        //given
+        Role role = createRole();
+
+        //when
+        roleRepository.save(role);
+        clear();
+
+        //then
+        Role foundRole = roleRepository.findById(role.getId()).orElseThrow(RoleNotFoundException::new);
+        assertThat(foundRole.getId()).isEqualTo(role.getId());
+    }
+
+    @Test
+    void deleteTest(){
+        //given
+        Role role = roleRepository.save(createRole());
+        clear();
+
+        //when
+        roleRepository.delete(role);
+
+        //then
+        assertThatThrownBy(() -> roleRepository.findById(role.getId()).orElseThrow(RoleNotFoundException::new))
+                .isInstanceOf(RoleNotFoundException.class);
+    }
+
+    @Test
+    void uniqueROleTypeTest(){
+        //given
+        roleRepository.save(createRole());
+        clear();
+
+        //when, then
+        assertThatThrownBy( () -> roleRepository.save(createRole()))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+
+    private Role createRole(){
+        return new Role(RoleType.ROLE_NORMAL);
+    }
+
+    private void clear(){
+        em.flush();
+        em.clear();
+    }
+}
